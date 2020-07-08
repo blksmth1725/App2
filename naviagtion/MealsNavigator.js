476a3ac59e248481b95fd2dc5c273a6d9fd60a1d
Christian Sheen <christiansheen@Christians-MacBook-Pro.local>
2020-07-07 23:41:33 -0400

    created my navigation container setup all screens that my navigation container need access to, then areated an appcontainer which wrapped around my mealsnavigator

---
+++ naviagtion/MealsNavigator.js
@@ -0,0 +1,14 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMeals from "../screens/CategoryMealsScreen";
+import MealDetailScreen from "../screens/MealDetailScreen";
+
+const MealsNavigator = createStackNavigator({
+	Categories: CategoriesScreen,
+	CategoryMeals: CategoryMeals,
+	MealDetail: MealDetailScreen,
+});
+
+export default createAppContainer(MealsNavigator);

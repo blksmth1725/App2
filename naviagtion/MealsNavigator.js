0329e4d80fc1a79e6ebf9e5cbe4b3312cb885a35
Christian Sheen <christiansheen@Christians-MacBook-Pro.local>
2020-07-08 06:11:57 -0400

    added tabs to the bottom of the main screen

--- naviagtion/MealsNavigator.js
+++ naviagtion/MealsNavigator.js
@@ -1,9 +1,12 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMeals from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
 
 import Colors from "../constant/Colors";
 
@@ -25,4 +28,20 @@ const MealsNavigator = createStackNavigator(
 	},
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+	{
+		Meals: {
+			screen: MealsNavigator,
+		},
+		Favorites: {
+			screen: FavoritesScreen,
+		},
+	},
+	{
+		tabBarOptions: {
+			activeTintColor: Colors.darkVioletColor,
+		},
+	},
+);
+
+export default createAppContainer(MealsFavTabNavigator);
